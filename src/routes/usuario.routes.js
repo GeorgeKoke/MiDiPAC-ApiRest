5287c2f9a40752330b7f4276160d62db0e995685
GeorgeKoke <jorge.contreras.palma@gmail.com>
2023-09-13 03:14:03 -0300

    Change routes v2

--- src/routes/usuario.routes.js
+++ src/routes/usuario.routes.js
@@ -3,10 +3,10 @@ import { readUsuarios, createUsuario, updateUsuario, deleteUsuario, readUsuario 
 
 const router = Router();
 
-router.get('GET/usuarios',readUsuarios);
-router.get('GET/usuario/:id?/:rut?',readUsuario);
-router.post('POST/usuario',createUsuario);
-router.put('UPDATE/usuario/:id',updateUsuario);
-router.delete('DELETE/usuario/:id',deleteUsuario);
+router.get('/get-usuarios',readUsuarios);
+router.get('/get-usuario/:id?/:rut?',readUsuario);
+router.post('/post-usuario',createUsuario);
+router.put('/put-usuario/:id',updateUsuario);
+router.delete('/del-usuario/:id',deleteUsuario);
 
 export default router;
